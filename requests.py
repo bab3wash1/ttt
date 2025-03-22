1b2d4493c4551639d7203ed273fdc206f8f9ac55
andreinekrasov59@gmail.com <andreinekrasov59@gmail.com>
2025-03-22 18:18:47 +0500

    req

--- requests.py
+++ requests.py
@@ -9,6 +9,7 @@ class TaskSchema(BaseModel):
     title: str
     completed: bool
     user: int
+
     model_config = ConfigDict(from_attributes=True)
 
 
@@ -35,8 +36,25 @@ async def get_tasks(user_id):
             TaskSchema.model_validate(t).model_dump() for t in tasks
         ]
 
-        return
+        return serialized_tasks
+
 
 async def get_completed_tasks_count(user_id):
     async with async_session() as session:
         return await session.scalar(select(func.count(Task.id)).where(Task.completed == True))
+
+
+async def add_task(user_id, title):
+    async with async_session() as session:
+        new_task = Task(
+            title=title,
+            user=user_id
+        )
+        session.add(new_task)
+        await session.commit()
+
+
+async def update_task(task_id):
+    async with async_session() as session:
+        await session.execute(update(Task).where(Task.id == task_id).values(completed=True))
+        await session.commit()
